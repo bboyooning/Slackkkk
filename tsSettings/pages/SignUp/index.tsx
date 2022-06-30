bd14287478f0f537fb078b65373aa8aa3e1b3023
bboyooning <bboyooning@gmail.com>
2022-06-30 18:35:44 +0900

    modify: 회원가입 기능구현 수정

--- tsSettings/pages/SignUp/index.tsx
+++ tsSettings/pages/SignUp/index.tsx
@@ -1,22 +1,19 @@
+import useInput from '@hooks/useInput';
+import axios from 'axios';
 import React, { useCallback, useState, VFC } from 'react';
 import { Link } from 'react-router-dom';
 import { Success, Form, Error, Label, Input, LinkContainer, Button, Header } from './styles';
 
 
 const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [nickname, setNickname] = useState('');
-  const [password, setPassword] = useState('');
-  const [passwordCheck, setPasswordCheck] = useState(''); 
+  const [email, onChangeEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, ,setPassword] = useInput('');
+  const [passwordCheck, ,setPasswordCheck] = useInput(''); 
   const [mismatchError, setMismatchError] = useState(false); 
+  const [signUpError, setSignUpError]= useState('');
+  const [signUpSuccess, setSignUpSuccess]= useState(false);
   
-  const onChangeEmail = useCallback((e)=>{
-    setEmail(e.target.value);
-  }, []);
-
-  const onChangeNickname = useCallback((e)=>{
-    setNickname(e.target.value);
-  }, []);
 
   const onChangePassword = useCallback((e)=>{
     setPassword(e.target.value);
@@ -30,8 +27,25 @@ const SignUp = () => {
   
   const onSubmit = useCallback((e) => {
     e.preventDefault();
-    if(!mismatchError){
+    if(!mismatchError && nickname){
       console.log('서버로 회원가입 하기')
+      setSignUpError(''); // (로딩 단계) 아래와 같이 비동기 요청 하기전에 초기화를 해주는 것이 좋음
+      setSignUpSuccess(false); // 마찬가지로 초기화!
+      // axios.post('http://localhost:3095/api/users', { // localhost:3090 -> :3095 에게 보내는 것
+      axios.post('/api/users', { // localhost:3095 -> localhost:3095 에게 보낸 것처럼 됨
+        email,
+        nickname,
+        password
+      })
+        .then((response)=>{ // 성공 단계
+          console.log(response);
+          setSignUpSuccess(true);
+        })
+        .catch((error)=>{ // 실패 단계
+          console.log(error.response);
+          setSignUpError(error.response.data); 
+        })
+        .finally(()=>{}); // finally 성공하던 실패하던 무조건 이 코드는 또 실행됨. try catch 문에도 생김!
     }
   }, 
   [email, nickname, password, passwordCheck, mismatchError]);
@@ -72,8 +86,8 @@ const SignUp = () => {
           </div>
           {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
           {!nickname && <Error>닉네임을 입력해주세요.</Error>}
-          {/* {signUpError && <Error>{signUpError}</Error>} */}
-          {/* {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+          {signUpError && <Error>{signUpError}</Error>}
+          {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}
         </Label>
         <Button type="submit">회원가입</Button>
       </Form>
